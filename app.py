ba21336fa452de1d3f3e928ff8aa4d25350cd516
sandaniez <it20231200@my.sliit.lk>
2023-10-26 22:34:26 +0530

    Github token updated

--- app.py
+++ app.py
@@ -416,7 +416,7 @@ def pf_home():
 
 def calculate_language_proficiency(username, access_token):
     # Authenticate with GitHub
-    access_token = "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"
+    access_token = "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"
     g = Github(access_token)
     user = g.get_user(username)
     
@@ -476,7 +476,7 @@ def plp_form():
 def plp():
     if request.method == 'POST':
         username = request.form.get('username')
-        access_token = "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"
+        access_token = "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"
         
         percentage_scores = calculate_language_proficiency(username, access_token)
         session['percentage_scores'] = percentage_scores
@@ -490,7 +490,7 @@ def plp():
 #----------------git-compare---------------------------
 def get_language_proficiency(username):
     # Authenticate with GitHub
-    access_token = "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"
+    access_token = "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"
     g = Github(access_token)
     user = g.get_user(username)
     
